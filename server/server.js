bf262dd3863f49022f11119bc14a69265ee56215
codewmanas <manas.kolaskar@somaiya.edu>
2025-06-29 11:04:09 +0530

    Added Routes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// Make the Server
 const app = express();
 
 // Connect Database
 connectDB();
 
+// Home Route
 app.get('/',(req, res) => {
     res.send('API, Running!');
 }
 );
 
+//  Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+// Host the Server on a Port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
