b62e85359cc05121f4201f3a9871df4d6b195b51
codewmanas <manas.kolaskar@somaiya.edu>
2025-07-13 15:15:54 +0530

    Add education experience

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import store from "./store";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 if (localStorage.token) {
@@ -80,6 +82,28 @@ const App = () => {
                   <section className="container">
                     <Alert />
                     <EditProfile />
+                  </section>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/add-experience"
+              element={
+                <PrivateRoute>
+                  <section className="container">
+                    <Alert />
+                    <AddExperience />
+                  </section>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/add-education"
+              element={
+                <PrivateRoute>
+                  <section className="container">
+                    <Alert />
+                    <AddEducation />
                   </section>
                 </PrivateRoute>
               }
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -62,3 +62,65 @@ export const createProfile =
       });
     }
   };
+
+// Add  Experience
+export const addExperience = (formData, navigate) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await axios.put("/api/profile/experience", formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert("Experience Added", "success"));
+    navigate("/dashboard");
+
+  } catch (error) {
+    if (error.response && error.response.data && error.response.data.errors) {
+      const errors = error.response.data.errors;
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response ? error.response.statusText : "Server Error",
+        status: error.response ? error.response.status : 500,
+      },
+    });
+  }
+}
+
+// Add Education
+export const addEducation = (formData, navigate) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await axios.put("/api/profile/education", formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert("Education Added", "success"));
+    navigate("/dashboard");
+
+  } catch (error) {
+    if (error.response && error.response.data && error.response.data.errors) {
+      const errors = error.response.data.errors;
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response ? error.response.statusText : "Server Error",
+        status: error.response ? error.response.status : 500,
+      },
+    });
+  }
+}
